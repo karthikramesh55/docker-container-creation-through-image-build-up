4001adcec1cc3db877f49c1751c32f79c166349c
Karthik Ramesh Kamath <31042314+karthikramesh55@users.noreply.github.com>
2025-11-30 15:24:12 +0000

    [file_modification] source code updation

--- kata_app.py
+++ kata_app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from datetime import datetime
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def kata_app_serving_functionality():
-    return "The kata app is currently running successfully"
+    data = {
+        "name": "Karthik Ramesh Kamath",
+        "datetime": datetime.now().isoformat(),
+        "journey": "Seeker"
+    }
+    return json.dumps(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000, debug=True)
